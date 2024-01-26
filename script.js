4ac9a8b64afd8bf8ce9106719a152590f6a1f0dd
syntact-1 <151948962+syntact-1@users.noreply.github.com>
2024-01-26 21:04:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function func(){
+    document.body.style.background = 'aliceblue'
+    document.body.style.transition = '.6s'
+}
+function func2(){
+    document.body.style.background = 'rgb(54, 51, 51)'
+    document.body.style.transition = '.6s'
+}
+
