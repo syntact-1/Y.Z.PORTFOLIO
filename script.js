3adc92ce2fd483f554b464544b6cc74c17739571
syntact-1 <151948962+syntact-1@users.noreply.github.com>
2024-02-02 21:20:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const btn_up = document.getElementById("btnup");
 function func(){
     document.body.style.background = 'aliceblue'
     document.body.style.transition = '.6s'
@@ -6,4 +7,10 @@ function func2(){
     document.body.style.background = 'rgb(54, 51, 51)'
     document.body.style.transition = '.6s'
 }
-
+btn_up.onclick = function(){
+    scroll({
+        top:0,
+        left:0,
+        behavior:"smooth"
+    })
+}
